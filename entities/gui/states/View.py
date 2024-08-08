b2a5f525d62806515ffa14699f640a4a9a579083
Jean Cunha <majornix051@gmail.com>
2024-08-08 10:30:25 -0300

    Creating view state

--- entities/gui/states/View.py
+++ entities/gui/states/View.py
@@ -11,7 +11,25 @@ class View(State):
         super().__init__(gui)
     
     def enter(self):
-        self.gui.grid_rowconfigure((0, 1, 2), weight=1)
+        self.gui.grid_rowconfigure((0, 1), weight=1)
+        self.gui.grid_columnconfigure((0, 1), weight=1)
+        self.gui.grid_columnconfigure(1, weight=5)
+
+        self.headerFrame = customtkinter.CTkFrame(master=self.gui)
+        self.headerFrame.grid_rowconfigure(0, weight=1)
+        self.headerFrame.grid_columnconfigure(1, weight=5)
+
+        self.returnButton = customtkinter.CTkButton(master=self.headerFrame, command=self.returnState) #ADD image later
+        self.title = customtkinter.CTkLabel(master=self.headerFrame, text="View")
+
+        self.headerFrame.grid(row=0, column=0, padx=20, pady=20, columnspan=2, sticky="nsew")
+        self.returnButton.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        self.title.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        self.mainFrame = customtkinter.CTkScrollableFrame(master=self.gui)
+        self.mainFrame.grid(row=1, column=0, columnspan=2, sticky="nsew")
+        self.mainFrame.grid_columnconfigure((0, 1, 2), weight=1)
+        self.mainFrame.grid_rowconfigure((0, 1, 2), weight=1)
         self.createBoxes([])
 
     def createBoxes(self, rules):
@@ -22,16 +40,25 @@ class View(State):
                 self.createBox(df, index, index % 3, index // 3)
 
     def createBox(self, df, index, i, j):
-        box = customtkinter.CTkFrame(master=self.gui)
+        box = customtkinter.CTkButton(master=self.mainFrame, text=df.at[index, "Name"], command=self.buttonCommand, height=400)
         box.grid(row=j, column=i, padx=20, pady=20, sticky="nsew")
-        # label = customtkinter.CTkLabel(master=box, text=row["Name"])
-        # label.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
-        image = Image.open(df.at[index, "ImagePath"])
-        w, h = image.size
-        cropedImage = image.crop((0, 0, w, 3 * w))
-        # image = tk.PhotoImage(file=df.at[index, "ImagePath"])
-        image = customtkinter.CTkImage(cropedImage, size=(400, 1200))
-        imageLabel = customtkinter.CTkLabel(master=box, image=image, text=df.at[index, "Name"])
-        imageLabel.pack(padx=20, pady=20)
+
+        originalText = df.at[index, "Name"]
+        hoverText = str(df.at[index, "Score"]) + "\n\n" + str(df.at[index, "Type"]) + "\n\n" + str(df.at[index, "Date"])
+
+        box.bind("<Enter>", lambda event, bt=box, ht=hoverText: self.showInfo(bt, ht))
+        box.bind("<Leave>", lambda event, bt=box, ot=originalText: self.hideInfo(bt, ot))
 
         return box
+    
+    def buttonCommand(self):
+        pass
+
+    def showInfo(self, button, hoverText):
+        button.configure(text=hoverText)
+
+    def hideInfo(self, button, originalText):
+        button.configure(text=originalText)
+
+    def returnState(self):
+        self.gui.setState(self.gui.add, type="Game")
